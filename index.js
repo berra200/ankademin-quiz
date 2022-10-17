d1cb5ac379d0079de7231ef22c7ba605790b94e6
Martin <martinbirgell@gmail.com>
2022-10-17 15:24:46 +0200

    Sorted some quick positioning

---
+++ index.js
@@ -0,0 +1,23 @@
+// Questions
+const questions = [
+    {
+        heading: "Fråga 1",
+        quesion: "vad är det här?",
+        answer: "en banan",
+    },
+    {
+        heading: "Fråga 2",
+        quesion: "vad är det här?",
+        answer: "en banan",
+    },
+    {
+        heading: "Fråga 3",
+        quesion: "vad är det här?",
+        answer: "en banan",
+    }
+];
+
+
+// Start
+
+let contentWrapper = document.querySelector("#contentWrapper");
